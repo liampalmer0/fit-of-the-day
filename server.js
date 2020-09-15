dcd468c178e153dfc7da82f9b1335c09d0a89121
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2020-09-15 14:03:42 -0400

    feat: setup placeholder homepage

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const http = require("http");
+const fs = require("fs");
 
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end("Hello world");
-  res.end();
-};
+const port = 8888;
+const address = "localhost";
 
-const server = http.createServer(requestListener);
-server.listen(8888);
+const server = http.createServer((req, res) => {
+  let rurl = req.url;
 
-console.log("Server started at localhost:8888");
+  console.log("Request: " + rurl);
+  
+  //If the request is for <filename>, read file and return data
+  //temporarily using explicit filenames
+  if (rurl === "/index.html" || rurl == "/") {
+    fs.readFile("./public/index.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  } else if (rurl == "/main.css") {
+    fs.readFile("./public/main.css", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.write(data);
+      return res.end();
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    return res.end("Error 404: Page not found");
+  }
+});
+
+server.listen(port, address, () => {
+  console.log(`Server running at ${address}:${port}`);
+});
