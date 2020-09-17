5ea4560cc80a1f10aa6f09669b1e77fc8b7ad355
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2020-09-17 11:06:45 -0400

    feat: start relative pathing
    Add dynamic access to allowed mime types in public dir
    Add title to index.html

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
 
 const port = 8888;
 const address = "localhost";
+const validmime = {
+  css: "text/css",
+  html: "text/html",
+  js: "text/javascript",
+  json: "application/json",
+  png: "image/png",
+  jpeg: "image/jpeg",
+};
 
 const server = http.createServer((req, res) => {
   let rurl = req.url;
 
   console.log("Request: " + rurl);
-  
+
   //If the request is for <filename>, read file and return data
-  //temporarily using explicit filenames
-  if (rurl === "/index.html" || rurl == "/") {
-    fs.readFile("./public/index.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  } else if (rurl == "/main.css") {
-    fs.readFile("./public/main.css", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/css" });
+  let rpath = buildPath(rurl);
+  if (rpath !== 1) {
+    fs.readFile(rpath[0], (err, data) => {
+      res.writeHead(200, {
+        "Content-Type": rpath[1],
+      });
       res.write(data);
       return res.end();
     });
@@ -32,3 +37,21 @@ const server = http.createServer((req, res) => {
 server.listen(port, address, () => {
   console.log(`Server running at ${address}:${port}`);
 });
+function buildPath(rurl) {
+  //check if extension is valid
+  let ext = path.extname(rurl);
+  if (validmime[ext.slice(1)] != undefined) {
+    console.log(`valid extension ${path.extname(rurl)}`);
+    let rpath = ["public/" + rurl, validmime[ext.slice(1)]];
+    //check if file exists
+    if (fs.existsSync(rpath[0])) {
+      return rpath;
+    } else {
+      console.log("file not found");
+      return 1;
+    }
+  } else {
+    console.log("invalid extension");
+    return 1;
+  }
+}
