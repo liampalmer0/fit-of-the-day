c5118318a5657eb7c8f08016bc278b715316eaae
blairtl <blairtl@mail.uc.edu>
2020-10-23 10:07:27 -0400

    feat: Set up closet.pug for rendering, updated database access

--- routes/closet.js
+++ routes/closet.js
@@ -19,11 +19,10 @@ function getArticles(usertoken, closetid) {
 
 function showCloset(req, res, next) {
   getArticles().then((result) => {
-    //result should be the object array from dbresult.rows
-    //if it doesnt exist then use empty object
+    console.log(result.rows);
     result = result ? result : {};
     result.pagename = 'closet';
-    res.render('closet', result);
+    res.render('closet', { articles: result.rows });
   });
 }
 
