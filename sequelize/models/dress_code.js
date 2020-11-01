64418dcb2e0316206f2a865160321039e7889f3a
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2020-11-01 00:02:17 -0400

    feat: create data models
    Add Sequelize ORM to project
    Create all basic models based on current db tables
    
    close #62

---
+++ sequelize/models/dress_code.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    'dress_code',
+    {
+      dress_code_id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      desc: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      tableName: 'dress_code',
+      schema: 'public',
+      timestamps: false,
+    }
+  );
+};
---
+++ sequelize/models/user.js
@@ -0,0 +1,52 @@
+const bcrypt = require('bcryptjs');
+const { Model } = require('sequelize');
+class User extends Model {
+  static async create(values) {
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(values.password, salt);
+    return super.create({
+      email: values.email,
+      username: values.username,
+      password: hash,
+    });
+  }
+}
+module.exports = (sequelize, DataTypes) => {
+  return User.init(
+    {
+      user_id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      fname: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      lname: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'user',
+      schema: 'public',
+      timestamps: false,
+    }
+  );
+};
