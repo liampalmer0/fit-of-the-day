1806f1abab85cf47e00049baebd835cdc7fc8b35
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2020-11-06 15:05:17 -0500

    refactor: split main sql script
    Split into create_main_tables and insert_main_data
    Update article model with filepath field

--- sequelize/models/article.js
+++ sequelize/models/article.js
@@ -56,6 +56,10 @@ module.exports = (sequelize, DataTypes) => {
           key: 'rating_id',
         },
       },
+      filepath: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
     {
       sequelize,
