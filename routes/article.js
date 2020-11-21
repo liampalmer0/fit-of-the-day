b6fbf383451ae6f41e6491fd0e3bcbe015afb6fa
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2020-11-20 19:44:57 -0500

    feat: user can add new article

--- routes/article.js
+++ routes/article.js
@@ -38,7 +38,59 @@ function showArticle(req, res, next) {
     });
 }
 
+function showCreate(req, res, next) {
+  let data = {};
+  data.action = 'new';
+  data.submitVal = 'Create';
+  res.locals.toParent = '../../';
+  res.render('create-article', data);
+}
+
+async function createArticle(req, res, next) {
+  //create article from req.body
+  let type = 0;
+  let dress_code = 0;
+  let filepath = '';
+
+  if (req.body.type === 'top') {
+    type = 1;
+    filepath = 's-null.png';
+  } else if (req.body.type === 'bottom') {
+    type = 2;
+    filepath = 'p-null.png';
+  } else {
+    type = 3;
+    filepath = 'p-null.png';
+  }
+
+  if (req.body.dress_code === 'casual') dress_code = 1;
+  else if (req.body.dress_code === 'semi-formal') dress_code = 2;
+  else dress_code = 3;
+
+  let dbres = await models.article.create({
+    closet_id: '4',
+    name: req.body.name,
+    desc: req.body.desc,
+    dirty: 'f',
+    garment_type_id: type,
+    color: req.body.color,
+    dress_code_id: dress_code,
+    rating_id: '5',
+    temp_min: '0',
+    temp_max: '100',
+    filepath: filepath,
+  });
+
+  res.redirect(
+    '../article?id=' + dbres.dataValues.article_id + '?success=true'
+  );
+}
+
 /* GET article page. */
 router.get('/', showArticle);
+
+router.get('/new', showCreate);
+
+router.post('/new', createArticle);
 
 module.exports = router;
