7e93dc1de780be429726ac694cafb8e27b2b7ce3
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2021-01-05 20:43:53 -0500

    refactor: add controllers & enhance queries
    Create article controller
    Improve closet controller
    Make queries validate username
    Add min max and dirty data to showArticle function

--- routes/closet.js
+++ routes/closet.js
@@ -1,29 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const articleRouter = require('./article');
-const { getArticles, getClosetId } = require('../controller/closetController');
-
-function showCloset(req, res, next) {
-  getClosetId(req.session.username)
-    .then((closetId) => {
-      return getArticles(req.session.username, closetId);
-    })
-    .then((query) => {
-      let data = {};
-      data.success = req.session.success ? req.session.success : false;
-      req.session.success = false;
-      data.title = 'FOTD - Closet';
-      data.pagename = 'closet';
-      data.articles = query;
-      res.render('closet', data);
-    })
-    .catch((err) => {
-      res.render('error', {
-        message: 'Closet Unavailable',
-        error: { status: err, stack: err.stack },
-      });
-    });
-}
+const { showCloset } = require('../controller/closetController');
 
 /* GET closet page. */
 router.get('/', showCloset);
