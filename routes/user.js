b8fa414265377c49efe64e2d909d06655b35ce64
Dabesu <ledd@mail.uc.edu>
2021-01-22 17:06:48 -0500

    style: nav bar rework
    closes #105 #106

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const dashboardRouter = require('./dashboard');
 const closetRouter = require('./closet');
-const settingsRouter = require('./settings');
+const accountRouter = require('./account');
 
 router.use((req, res, next) => {
   res.locals.toParent = '';
@@ -11,7 +11,7 @@ router.use((req, res, next) => {
 });
 
 router.use('/dashboard', dashboardRouter);
-router.use('/settings', settingsRouter);
+router.use('/account', accountRouter);
 router.use('/closet', closetRouter);
 
 module.exports = router;
