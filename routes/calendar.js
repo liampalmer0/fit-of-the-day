c3921d56b6b7e68fd4eb0d8d688f73ed6b0b8620
blairtl <blairtl@mail.uc.edu>
2021-03-05 17:11:16 -0500

    feat: Event creation

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const router = express.Router();
-// const { models } = require('../sequelize');
-const { getCalendarFrag } = require('../controller/calendarController');
+const {
+  getCalendarFrag,
+  saveEvent
+} = require('../controller/calendarController');
 
 /* GET calendar page. */
 router.get('/', (req, res) => {
   res.render('calendar-page', { pagename: 'calendar' });
 });
-/* POST ajax for adding cal event. */
-router.post('/add', (req, res) => {
-  // add event with data to db
-  // res.render calendar body pug fragment with updated data
-  res.send('you just posted for a new calendar event');
-});
+
+router.post('/newEvent', saveEvent);
 
 router.get('/events', getCalendarFrag);
 
